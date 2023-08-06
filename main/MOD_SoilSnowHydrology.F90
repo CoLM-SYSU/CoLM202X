de106c1b20c7c2fa9b5ee1b556e88f936ac4b3d0
zhangsp8 <21294631@qq.com>
2023-08-06 22:47:42 +0800

    Bug fixed

--- main/MOD_SoilSnowHydrology.F90
+++ main/MOD_SoilSnowHydrology.F90
@@ -197,12 +197,14 @@ MODULE MOD_SoilSnowHydrology
          ENDIF
       endif
 
+#ifdef CROP
       if(DEF_USE_IRRIGATION)then
          ps = patch_pft_s(ipatch)
          pe = patch_pft_e(ipatch)
          call CalIrrigationApplicationFluxes(ipatch,ps,pe,deltim,qflx_irrig_drip,qflx_irrig_sprinkler,qflx_irrig_flood,qflx_irrig_paddy)
          gwat = gwat + qflx_irrig_drip + qflx_irrig_flood + qflx_irrig_paddy
       end if
+#endif
 
 !=======================================================================
 ! [2] surface runoff and infiltration
@@ -577,12 +579,14 @@ MODULE MOD_SoilSnowHydrology
          ENDIF
       endif
 
+#ifdef CROP
       if(DEF_USE_IRRIGATION)then
          ps = patch_pft_s(ipatch)
          pe = patch_pft_e(ipatch)
          call CalIrrigationApplicationFluxes(ipatch,ps,pe,deltim,qflx_irrig_drip,qflx_irrig_sprinkler,qflx_irrig_flood,qflx_irrig_paddy)
          gwat = gwat + qflx_irrig_drip + qflx_irrig_flood + qflx_irrig_paddy
       end if
+#endif
 !=======================================================================
 ! [2] surface runoff and infiltration
 !=======================================================================
