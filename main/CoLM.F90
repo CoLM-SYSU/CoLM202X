6affcc750763be1df71398f839b940042016b400
Lu Li <lilu83@mail.sysu.edu.cn>
2024-08-31 10:25:54 +0800

    Update CoLM.F90
    1. Add USESplitAI to split process for Python and Fortran (must use GNU compiler)
    
    Co-Authored-By: Chen Sisi <115998395+csscn@users.noreply.github.com>

--- main/CoLM.F90
+++ main/CoLM.F90
@@ -132,7 +132,20 @@ PROGRAM CoLM
    integer*8 :: start_time, end_time, c_per_sec, time_used
 
 #ifdef USEMPI
-      CALL spmd_init ()
+#ifdef USESplitAI
+      integer :: num_procs, my_rank, ierr, color, new_comm
+      call MPI_Init(ierr) ! Initialize MPI
+      call MPI_Comm_size(MPI_COMM_WORLD, num_procs, ierr) ! Get the total number of processes
+      call MPI_Comm_rank(MPI_COMM_WORLD, my_rank, ierr) ! Get the rank of the current process
+      color = 1 ! The pyroot process will be in its own communicator
+      print*, 'before split I am process', my_rank, 'of', num_procs
+      call MPI_Comm_split(MPI_COMM_WORLD, color, my_rank, new_comm, ierr) ! Split the communicator
+      print*, 'after split I am process', my_rank, 'of', num_procs
+      call MPI_Comm_size(new_comm, num_procs, ierr) ! Get the total number of processes
+      call MPI_Comm_rank(new_comm, my_rank, ierr) ! Get the rank of the current process
+      print*,num_procs,"for CoLM"
+#endif
+      CALL spmd_init (new_comm)
 #endif
 
       CALL getarg (1, nlfile)
