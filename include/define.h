19177da6b91b3e932af44a74038c10d5cbd66681
Hua Yuan <yuanh25@mail.sysu.edu.cn>
2024-12-23 19:53:34 +0800

    Add dependences for running URBAN_MODEL.
    -add(include/define.h):
        add dependences for URBAN model in define.h.

--- include/define.h
+++ include/define.h
@@ -14,6 +14,12 @@
 
 ! 2.1 3D Urban model (put it temporarily here):
 #undef URBAN_MODEL
+#if (defined URBAN_MODEL && defined SinglePoint)
+#undef LULC_USGS
+#define LULC_IGBP
+#undef LULC_IGBP_PFT
+#undef LULC_IGBP_PC
+#endif
 
 ! 3. If defined, debug information is output.
 #define CoLMDEBUG
@@ -64,10 +70,10 @@
 #undef DataAssimilation
 
 ! 10. Vector write model.
-!     1) "VectorInOneFileP" : write vector data in one file in parallel mode;  
-!     2) "VectorInOneFileS" : write vector data in one file in serial mode;  
-!     3) Neither "VectorInOneFileS" nor "VectorInOneFileP" is defined : 
-!        write vector data in separate files.  
+!     1) "VectorInOneFileP" : write vector data in one file in parallel mode;
+!     2) "VectorInOneFileS" : write vector data in one file in serial mode;
+!     3) Neither "VectorInOneFileS" nor "VectorInOneFileP" is defined :
+!        write vector data in separate files.
 #undef VectorInOneFileP
 !     Conflict
 #ifdef VectorInOneFileP
